5682c823b4ca143a9d019ba11a8caf30db144050
ANSABUDDIN <ansabuddin0346@gmail.com>
2023-08-16 18:43:13 +0500

    new work on cv added

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,34 +1,69 @@
 <template>
+  <div>
     <div>
-      <!-- Display template content -->
-      <pre>{{ templateContent }}</pre>
-  
-      <!-- Button to generate code -->
-      <button @click="generateCode">Generate Code</button>
+      <label for="title">Title:</label>
+      <input v-model="data.title" id="title" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        templateContent: '',
-      };
-    },
-    methods: {
-      generateCode() {
-        // Make API request to fetch template based on templateId
-        const templateId = 'template1'; // Replace with actual template ID
-        fetch(`http://localhost:4000/api/templates/${templateId}`)
-          .then(response => response.json())
-          .then(data => {
-            console.log("data" ,data)
-            // Display generated code
-            this.templateContent = data.generatedCode;
-          })
-          .catch(error => console.error(error));
-      },
-    },
-  };
-  </script>
-  
+    <div>
+      <label for="designation">designation:</label>
+      <input v-model="data.designation" id="designation" />
+    </div>
+    <div>
+      <label for="content">Content:</label>
+      <textarea v-model="data.content" id="content"></textarea>
+    </div>
+    <button @click="fetchTemplate">Load Template</button>
+
+    <div v-if="templateLoaded" v-html="formattedTemplate"></div>
+    <div v-else>Loading template...</div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, computed, reactive } from 'vue';
+
+export default {
+  setup() {
+    const template = ref('');
+    const templateLoaded = ref(false);
+    const data = reactive({
+      title: '',
+      content: '',
+      designation : '',
+    });
+
+    const fetchTemplate = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/template');
+        console.log(response);
+        template.value = await response.text();
+        templateLoaded.value = true;
+      } catch (error) {
+        console.error('Error fetching template:', error);
+      }
+    };
+
+    const formattedTemplate = computed(() => {
+      if (!templateLoaded.value) {
+        return '';
+      }
+      // Replace placeholders with actual data
+      return template.value
+        .replace('{{ title }}', data.title)
+        .replace('{{ content }}', data.content)
+        .replace('{{ designation }}', data.designation);
+    });
+
+    onMounted(() => {
+      fetchTemplate();
+    });
+
+    return {
+      formattedTemplate,
+      templateLoaded,
+      fetchTemplate,
+      data
+    };
+  }
+};
+</script>
